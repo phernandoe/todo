9e6a127ce6516d031eb5b2bc7c65aaa972116ea7
Phernandoe <fbarreto@ithaca.edu>
2019-11-11 14:48:15 -0400

    help button styling

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Help = () => (
-  <div className='help'>
-    <h4>?</h4>
-    <ul className='help-commands'>
-      <li className='help-command'>add(name)</li>
-      <li className='help-command'>del(id)</li>
-      <li className='help-command'>rn(id, new name)</li>
-      <li className='help-command'>com(id)</li>
-    </ul>
-  </div>
-);
+const Help = () => {
+
+  const [isOpen, setOpen] = useState(true);
+
+  const handleClick = () => setOpen(!isOpen);
+
+  if (isOpen) {
+    return (
+      <div className='help'>
+        <button aria-label='help button' className='help-button' onClick={handleClick}>?</button>
+        <ul className='help-commands'>
+          <li className='help-command'>add(name)</li>
+          <li className='help-command'>del(id)</li>
+          <li className='help-command'>rn(id, new name)</li>
+          <li className='help-command'>com(id)</li>
+        </ul>
+      </div>
+    )
+  } else {
+    return (
+      <div className='help'>
+        <button aria-label='help button' className='help-button' onClick={handleClick}>?</button>
+      </div>
+    )
+  }
+  
+};
 
 export default Help;
